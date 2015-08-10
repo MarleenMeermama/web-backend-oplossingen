ccbffb925fceb1ecfe5f2470b19beb4a8743d21c
Marleen <LocalAdmin@MECH-B107-09.syntraab.mechelen>
2015-08-10 13:20:51 +0200

    opdracht comments deel 1 toevoegen

---
+++ opdracht-comment/opdracht1.php
@@ -0,0 +1,27 @@
+<?php
+$voornaam = ' Marleen';
+$naam = ' Meermans';
+
+
+/*
+* Marleen Meermans
+*marleen.meermans@gmail.com
+*/
+
+?>
+
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+
+<h1>Opdracht Comments: deel1</h1>
+<p>Mijn voornaam is<?= $voornaam ?></p>
+<p>Mijn achternaam is<?= $naam ?></p>
+
+
+
+</body>
+</html>
